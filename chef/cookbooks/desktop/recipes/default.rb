0551f49aba322410e243b03c45a6aac6fe7af335
Jim Rosser <jarosser06@gmail.com>
2014-08-10 19:51:25 -0500

    more

---
+++ chef/cookbooks/desktop/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: desktop
+# Recipe:: default
+#
+# Copyright 2014, Jim Rosser
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w(
+  elixir
+  gdb
+  libxml2-devel
+  libxslt-devel
+  gcc-c++
+  gcc
+  make
+  cmake
+  git
+  mercurial
+  bison
+  docker-io
+  irssi
+).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+service 'docker' do
+  action %i(enable start)
+end
