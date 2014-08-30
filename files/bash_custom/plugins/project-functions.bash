48383ce12db97379804d5e70144a01c47a3dc6ee
Jim Rosser <jarosser06@gmail.com>
2014-08-30 15:14:01 -0500

    removed .git, fixed github_update function

--- files/bash_custom/plugins/project-functions.bash
+++ files/bash_custom/plugins/project-functions.bash
@@ -33,23 +33,29 @@ function clone_github_project() {
     return 0
   else
     pushd $PROJECTS_DIR &> /dev/null
-    git clone git@github.com:${project}
+    echo $project
+    echo "git clone git@github.com:${project}"
+    #git clone git@github.com:${project}
     popd &> /dev/null
     return 0
   fi
 }
 
-function update_projects() {
-  hosting_site=$1
-
-  if [ -z $hosting_site ]; then
-    echo "You must pass a project domain"
-    return 1
-  fi
-
-  project_list=$(jq ".${hosting_site}[]" ${HOME}/.projects.json)
+function update_github_projects() {
+  project_list=$(jq ".github[]" ${HOME}/.projects.json)
   for project in $project_list;
   do
-    update_project $project
+    ## Strip the first and last quote
+    project="${project%\"}"
+    project="${project#\"}"
+
+    ## Strip the org name
+    prj_name=${project##*/}
+
+    if [ -d ${PROJECTS_DIR}/${prj_name} ]; then
+      update_project $prj_name
+    else
+      clone_github_project $project
+    fi
   done
 }
