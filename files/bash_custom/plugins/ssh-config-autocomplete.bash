d49b75150150123db5c5f8048b1bdf1040c4ac49
Jim Rosser <jarosser06@gmail.com>
2014-08-30 15:37:31 -0500

    autocomplete for ssh-config

---
+++ files/bash_custom/plugins/ssh-config-autocomplete.bash
@@ -0,0 +1,12 @@
+_ssh_config()
+{
+  _script_commands="list use"
+
+  local cur prev
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+
+  return 0
+}
+complete -o nospace -F _ssh_config ssh-config
