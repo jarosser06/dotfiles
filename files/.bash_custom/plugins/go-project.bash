b911f6d9df76160ec71ec8125f8eb19725c8b65f
Jim Rosser <jarosser06@gmail.com>
2015-03-21 22:37:24 -0500

    fixed go-project.bash

--- files/.bash_custom/plugins/go-project.bash
+++ files/.bash_custom/plugins/go-project.bash
@@ -1,22 +1,22 @@
-#bin/bash
+#!/bin/bash
 
 function goproject() {
   if [ -z $1 ]; then
     echo "Must pass a project name"
   else
     PROJ_NAME=$1
-    PROJ_BASE_DIR=${GOPATHsrc/github.com/jarosser06
-    PROJ_DIR=${PROJ_BASE_DIR${PROJ_NAME}
-    mkdir -p $PROJ_BASE_DIR &>dev/null
-    git clone git@github.com:jarosser0goproject.git $PROJ_DIR &> /dev/null
+    PROJ_BASE_DIR=${GOPATH}/src/github.com/jarosser06
+    PROJ_DIR=${PROJ_BASE_DIR}/${PROJ_NAME}
+    mkdir -p $PROJ_BASE_DIR &> /dev/null
+    git clone git@github.com:jarosser06/goproject.git $PROJ_DIR &> /dev/null
 
     cd $PROJ_DIR
-    rm -rf .git &>dev/null
+    rm -rf .git &> /dev/null
 
-    PROJ_TEMPLATES="README.md scriptmake.sh"
+    PROJ_TEMPLATES="README.md scripts/make.sh"
     for temp in $PROJ_TEMPLATES
     do
-      sed -i GOPROJECT/${PROJ_NAME}/g ${temp}
+      sed -i s/GOPROJECT/${PROJ_NAME}/g ${temp}
     done
     git init
 
