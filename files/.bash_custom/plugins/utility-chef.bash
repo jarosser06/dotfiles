e202d6078074496fc7facf35474290edffb593ce
Jim Rosser <jarosser06@gmail.com>
2015-05-20 15:41:16 -0500

    added purge_cookbooks function to chef utility

--- files/.bash_custom/plugins/utility-chef.bash
+++ files/.bash_custom/plugins/utility-chef.bash
@@ -7,3 +7,12 @@ function cookbook() {
 function activate_chefdk() {
   eval "$(chef shell-init bash)"
 }
+
+function purge_cookbooks() {
+  cookbooks=$(knife cookbook list | awk '{ print $1 }')
+
+  for cookbook in $cookbooks
+  do
+    knife cookbook delete $cookbook -a -y
+  done
+}
