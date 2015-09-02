b0162c400a20ebd89accece4f5760f1c749a0638
Jim Rosser <jarosser06@gmail.com>
2015-09-01 22:13:20 -0500

    added source_if_exists bash function

--- files/.bash_custom/plugins/utility.bash
+++ files/.bash_custom/plugins/utility.bash
@@ -8,6 +8,14 @@ function update_dotfiles {
   popd &> /dev/null
 }
 
+function source_if_exists {
+  sh_file=$1
+
+  if [ -a $sh_file ]; then
+    source $sh_file
+  fi
+}
+
 function trim_branches {
   git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
 }
