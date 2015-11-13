4cfbf8087ff59f08ebe5b4c9192132625d81947f
Jim Rosser <jarosser06@gmail.com>
2015-11-13 09:29:02 -0600

    Added support for nested project names in goproject

--- files/.bash_custom/plugins/go-project.bash
+++ files/.bash_custom/plugins/go-project.bash
@@ -4,19 +4,25 @@ function goproject() {
   if [ -z $1 ]; then
     echo "Must pass a project name"
   else
-    PROJ_NAME=$1
-    PROJ_BASE_DIR=${GOPATH}/src/github.com/jarosser06
-    PROJ_DIR=${PROJ_BASE_DIR}/${PROJ_NAME}
+    PROJ_PATH=$1
+    PROJ_BASE_DIR=${GOPATH}/src/github.com/
+    PROJ_DIR=${PROJ_BASE_DIR}/${PROJ_PATH}
     mkdir -p $PROJ_BASE_DIR &> /dev/null
     git clone git@github.com:jarosser06/goproject.git $PROJ_DIR &> /dev/null
 
     cd $PROJ_DIR
     rm -rf .git &> /dev/null
 
+    # Split the path into an array to grab the name
+    IFS='/' read -r -a split_name <<< $PROJ_PATH
+
+    # Get the name of the project
+    PROJ_NAME=${split_name[-1]}
+
     PROJ_TEMPLATES="README.md Makefile"
     for temp in $PROJ_TEMPLATES
     do
-      sed -i s/GOPROJECT/${PROJ_NAME}/g ${temp}
+     sed -i "s/GOPROJECT/${PROJ_NAME}/g" ${temp}
     done
     git init
 
