a725004ea4b143cc62a809e32464aa0c107dcd1b
Jim Rosser <jarosser06@gmail.com>
2023-03-24 09:14:46 -0500

    Add support for quick AWS acct identification in prompt

--- files/.bash_custom/plugins/aws.bash
+++ files/.bash_custom/plugins/aws.bash
@@ -12,5 +12,29 @@ function s3-ls-bucket() {
   aws s3 ls --recursive $bucket
 }
 
+##### Account Shortcuts #####
+function active_aws_account() {
+  which jq &> /dev/null
+  if [[ ${?} -ne 0 ]]; then
+    return 1
+  fi
+
+  local acct
+  acct=$(aws sts get-caller-identity | jq .Account | sed 's/"//g')
+  if [[ ${?} -eq 0 ]]; then
+    echo ${acct}
+  fi
+
+  return 0
+}
+
+PS_ACTIVE_AWS_ENABLED=0
+function __aws_active_acct_ps() {
+  if [[ ${PS_ACTIVE_AWS_ENABLED} -ne 1 ]]; then
+    return 0
+  fi
+  echo " [AWS:$(active_aws_account)]"
+}
+
 ##### CloudFormation Shortcuts #####
 alias cf-ls-stacks="aws cloudformation describe-stacks | jq '.Stacks[] | .StackName + \" - \" + .StackStatus' | sed -e 's/^\"//' -e 's/\"$//'"
